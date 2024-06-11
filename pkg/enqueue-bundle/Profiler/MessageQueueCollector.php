7b8f93ca9653e75a62fa6b2b0d3d6f05ce9d9186
Nikita Bolotov <nikita.bolotov@betterme.world>
2024-06-11 19:03:17 +0300

    local repo tests

--- pkg/enqueue-bundle/Profiler/MessageQueueCollector.php
+++ pkg/enqueue-bundle/Profiler/MessageQueueCollector.php
@@ -10,7 +10,7 @@ class MessageQueueCollector extends AbstractMessageQueueCollector
     /**
      * @return void
      */
-    public function collect(Request $request, Response $response, ?\Throwable $exception = null)
+    public function collect(Request $request, Response $response, ?\Throwable $exception = null): void
     {
         $this->collectInternal($request, $response);
     }
